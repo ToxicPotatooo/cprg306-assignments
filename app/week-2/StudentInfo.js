cb5e6ab1bbdd7970858c79043f5c6b6d68b035ec
ToxicPotatooo <edwin.wong@edu.sait.ca>
2026-01-23 22:48:24 -0700

    Changed page.js to add link to week 2 and edited studentinfo.js

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,18 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Edwin Wong</p>
+      <p>
+        GitHub:{" "}
+        <a
+          href="https://github.com/ToxicPotatooo/cprg306-assignments"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "underline" }}
+        >
+          ToxicPotatooo/cprg306-assignments
+        </a>
+      </p>
+    </div>
+  );
+}
