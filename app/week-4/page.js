a9d5a37c228962636118d78df119be129f4cc1e5
ToxicPotatooo <edwin.wong@edu.sait.ca>
2026-02-06 16:16:28 -0700

    Finalized styling

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 export default function Page() {
   return (
     <main className="max-w-md mx-auto p-4">
-      <h1 className="text-2xl font-bold text-gray-800 mb-4">Shopping List</h1>
+      <h1 className="text-2xl font-bold text-white mb-4">Shopping List</h1>
       <ItemList />
     </main>
   );
