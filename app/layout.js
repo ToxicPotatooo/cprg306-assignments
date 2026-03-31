15771cc2a1064a59e26ba895b8d251cee07833b8
ToxicPotatooo <edwin.wong@edu.sait.ca>
2026-03-30 19:00:00 -0600

    Fixed layout.js error

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthContextProvider } from "./contexts/AuthContext.js";
- 
-export default function Layout({ children }) {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
-}
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
   );
